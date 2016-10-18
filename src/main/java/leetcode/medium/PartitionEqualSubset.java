86b1bee4eea5f83a5a2788307c5414aed50a94a9
Phani <pv029500@cerner.net>
2016-10-17 21:59:18 -0500

    Adding recursion to wordbreak

---
+++ src/main/java/leetcode/medium/PartitionEqualSubset.java
@@ -0,0 +1,7 @@
+package leetcode.medium;
+
+/**
+ * Created by PV029500 on 10/14/2016.
+ */
+public class PartitionEqualSubset {
+}
---
+++ src/test/java/leetcode/medium/PartitionEqualSubsetTest.java
@@ -0,0 +1,10 @@
+package leetcode.medium;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by PV029500 on 10/14/2016.
+ */
+public class PartitionEqualSubsetTest {
+
+}
