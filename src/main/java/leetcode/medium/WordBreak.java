b28686c13c81f027165686264624896b3bb4b50f
Phani <pv029500@cerner.net>
2016-10-17 22:07:45 -0500

    Adding recursion to wordbreak

--- src/main/java/leetcode/medium/WordBreak.java
+++ src/main/java/leetcode/medium/WordBreak.java
@@ -26,9 +26,9 @@ public class WordBreak {
     public boolean recursive(String s, Set<String> wordDict) {
 
         if(s.equals(""))return true;
-        for(int i=0;i<s.length();i++){
-            if(wordDict.contains(s.substring(0,i+1))){
-                if(wordBreak(s.substring(i+1), wordDict))return true;
+        for(int i = 0; i < s.length(); i++){
+            if(wordDict.contains(s.substring(0, i + 1))){
+                if(wordBreak(s.substring(i + 1), wordDict))return true;
             }
         }
         return false;
