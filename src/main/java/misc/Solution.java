73ac9ab2f6a2bd56d17d842f3b1ae3797c700834
Phani <pv029500@cerner.net>
2017-01-03 23:57:51 -0600

    unknown changes

---
+++ src/main/java/misc/Solution.java
@@ -0,0 +1,35 @@
+package misc;
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        int operations = scanner.nextInt();
+        int[] updates = new int[3];
+        int[] res = new int[length];
+        for (int j = 0; j < operations; j++) {
+
+            int start = scanner.nextInt() - 1;
+            int end = scanner.nextInt() - 1;
+            int value = scanner.nextInt();
+            res[start] += value;
+            Class in = Integer.class;
+            if(end < length - 1)
+                res[end + 1] -= value;
+
+
+        }
+
+        int sum = 0, max = 0;
+        for(int i = 0; i < length; i++) {
+            sum += res[i];
+            res[i] = sum;
+            max = max > sum ? max : sum;
+        }
+
+        System.out.println(max);
+    }
+
+}
--- src/test/java/epi/primitivetypes/MultipleTwoNumTest.java
+++ src/test/java/epi/primitivetypes/MultipleTwoNumTest.java
@@ -2,6 +2,8 @@ package epi.primitivetypes;
 
 import org.junit.Test;
 
+import java.util.Scanner;
+
 /**
  * Created by PV029500 on 12/28/2016.
  */
@@ -9,10 +11,21 @@ public class MultipleTwoNumTest {
 
     @Test
     public void MultipleTest() {
-        MultiplyTwoNum multiplyTwoNum = new MultiplyTwoNum();
-        for (int i = 1; i < 10; i++)
-            for (int j = 1; j < 10; j++)
-                assert multiplyTwoNum.multiplyXandY(i, j) == i * j;
+//        MultiplyTwoNum multiplyTwoNum = new MultiplyTwoNum();
+//        for (int i = 1; i < 10; i++)
+//            for (int j = 1; j < 10; j++)
+//                assert multiplyTwoNum.multiplyXandY(i, j) == i * j;
+//        TreeMap m = new TreeMap();
+//
+        int[] a = {1};
+        MultipleTwoNumTest multipleTwoNumTest = new MultipleTwoNumTest();
+        multipleTwoNumTest.increment(a);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(a[a.length - 1]);
+    }
+
+    public void increment(int[] i) {
+        i[i.length - 1]++;
     }
 
 }
